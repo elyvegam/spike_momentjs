e67949fa532b43af0b241c1cf10d1002fbe4f998
elyvegam <116033455+elyvegam@users.noreply.github.com>
2023-01-25 18:17:53 -0300

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -2,15 +2,6 @@ import { DateTime, Settings } from 'luxon';
 
 Settings.defaultLocale = 'es-mx'; // seteo global
 
-const months =
-  'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre';
-const monthsShort = 'Ene._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.';
-const weekdays = 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado';
-const weekdaysShort = 'Dom._Lun._Mar._Mie._Jue._Vie._Sab.';
-const weekdaysMin = 'Do_Lu_Ma_Mi_Ju_Vi_Sa';
-const monthsInLoweCase =
-  'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre';
-
 const formatDate = (date, format) => {
   const dt = DateTime.fromISO(date); // necesario al utilizar ISO
   let formatedDate;
@@ -27,12 +18,14 @@ const formatDate = (date, format) => {
         formatedDate = dt.toFormat('DDD');
         return formatedDate;
       case 'weekday_date':
-        formatedDate = `${`${dt.toFormat('cccc')} ${dt.toFormat('dd')}`}`;
+        formatedDate = dt.toFormat('cccc dd');
         return formatedDate;
       case 'day_month_year_hour_minute':
-        formatedDate = `${dt.toFormat('DDD')} a ${
-          dt.toFormat('hh') === '01' ? 'la' : 'las'
-        } ${dt.toFormat('t')} ${dt.toFormat('a') === 'a. m' ? 'AM' : 'PM'}`;
+        const useSingularOrPlural = dt.toFormat('hh') === '01' ? 'la' : 'las';
+        const useAMorPM = dt.toFormat('a') === 'a. m' ? 'AM' : 'PM';
+        formatedDate = dt.toFormat(
+          `DDD 'a ${useSingularOrPlural}' t '${useAMorPM}'`
+        );
         return formatedDate;
       default:
         throw formatoInvalido;
@@ -53,7 +46,7 @@ console.log(
 console.log(
   'day_month_year_hour_minute singular',
   formatDate('2022-08-26T11:00:01-06:00', 'day_month_year_hour_minute')
-); // 26 de agosto de 2022 a la 13:00 PM (a pesar que la documentación refiere a query AM/PM el resultado no es el mencionado)
+); // 26 de agosto de 2022 a la 13:00 PM (a pesar que la documentación refiere a query AM/PM el resultado no es el mencionado, pasa igual con formato 12 horas)
 console.log(
   'day_month_year_hour_minute plural',
   formatDate('2022-08-26T10:52:03-06:00', 'day_month_year_hour_minute') // 26 de agosto de 2022 a las 12:52 PM
